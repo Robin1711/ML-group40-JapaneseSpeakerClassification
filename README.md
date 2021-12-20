8c77c8ac728a4a9a977d263e088f015b330d16c1
a-krauss <a.krauss.1@student.rug.nl>
2021-12-20 23:58:35 +0100

    data loader (partially unfinished)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ML-group40-JapaneseSpeakerClassification
 Our magnificent project for the machine learning course 2021. We will utilize machine learning approaches to classify speakers of a Japanese vowel.
+
+# Data Explained
+There are **9** speakers who have uttered the /ae/ japanease vowel successively. This data is recorded in timesteps (10 to 30 roughly) for the duration of the utterance across 12 chanels (smth like frequency ranges).
+
+For some reason the the data labels each individual timestep looking ath the included matlab data importer. Though the coloum is what actually identifies the speaker.
---
+++ data_loader.py
@@ -0,0 +1,86 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+
+train_data_path = './data/ae.train'
+test_data_path = './data/ae.test'
+
+def read_raw_file_lines(filename):
+    with open(filename) as f:
+        contents = f.readlines()
+    return contents
+
+def proccess_x(dataset):
+    x = []
+    start_idx = 0
+    for idx, line in enumerate(dataset):
+        if line[0:4] != '1.0 ':
+            continue
+        else:
+            utterance_str = dataset[start_idx:idx]
+            utterance = []
+            for str_line in utterance_str:
+                feature = [float(num) for num in str_line.strip().split(" ") if num != ""]
+                utterance.append(feature)
+            x.append(utterance)
+            start_idx = idx + 1
+    return x
+
+def proccess_y_timestep(dataset):
+    y = []
+    for i in range(len(dataset)):
+        l = len(dataset[i])
+        labels = np.zeros((l,9))
+        speakerIndex = np.ceil(i/30)
+        ## unfinished
+
+        
+# To be implemented
+def proccess_y_timeseries(dataset):
+    pass
+
+## this function does what the provided matlab dataloader does: one hot encoding for each timestep of the utterance
+def load_w_label_timestep():
+    raw_train = read_raw_file_lines(train_data_path)
+    raw_test = read_raw_file_lines(test_data_path)
+
+    train_x = np.asarray(proccess_x(raw_train),dtype=object)
+    test_x = np.asarray(proccess_x(raw_test),dtype=object)
+    
+    train_y = proccess_y_timestep(train_x)
+    test_y = proccess_y_timestep(test_x)
+
+    return [train_x, train_y, test_x, test_y]
+
+
+
+#trainInputs = cell(270,1);
+#readindex = 0;
+#for c = 1:270
+#    readindex = readindex + 1;
+#    l = 0;    
+#    while aeTrain(readindex, 1) ~= 1.0
+#        l = l+1;
+#        readindex = readindex + 1;
+#    end
+#    trainInputs{c,1} = aeTrain(readindex-l:readindex-1,:);    
+#end
+
+## this function returns the data with labels for each time searies, also one hot encoding.
+def load_w_label_timeseries():
+    raw_train = read_raw_file_lines(train_data_path)
+    raw_test = read_raw_file_lines(test_data_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Load train/test data in a specific format')
+    parser.add_argument('-labels_per_timestep', type=bool, default=False, help='if true each time step of the utterance has a label, otherwise the label is associated with the entire timeserise of the utterance.')
+
+    args = parser.parse_args()
+
+    if args.labels_per_timestep:
+        print("Loading data with timestep labels")
+        load_w_label_timestep()
+    else:
+        print("Loading data with timeseries labels")
+        load_w_label_timeseries()
+    
