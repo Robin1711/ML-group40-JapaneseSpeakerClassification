2ffa97cbd609002891fcc9ced69e62c924fc4394
a-krauss <a.krauss.1@student.rug.nl>
2021-12-24 13:20:09 +0400

    dataloading with timestep+timeseries labels done

--- data_loader.py
+++ data_loader.py
@@ -26,17 +26,23 @@ def proccess_x(dataset):
             start_idx = idx + 1
     return x
 
-def proccess_train_y_timestep(dataset):
+def proccess_train_y(dataset, timestep = True):
     y = []
     for i in range(len(dataset)):
-        l = len(dataset[i])
-        labels = np.zeros((l,9))
         speakerIndex = int(np.floor(i/30))
-        labels[:,speakerIndex] = 1
+
+        if timestep == True:
+            l = len(dataset[i])
+            labels = np.zeros((l,9))
+            labels[:,speakerIndex] = 1
+        else:
+            labels = np.zeros(9)
+            labels[speakerIndex] = 1
+
         y.append(labels)
     return y
 
-def proccess_test_y_timestep(dataset):
+def proccess_test_y(dataset, timestep = True):
     y = []
     speakerIndex = 0
     blockCounter = 0
@@ -46,15 +52,15 @@ def proccess_test_y_timestep(dataset):
         if blockCounter == blockLengthes[speakerIndex] + 1:
             speakerIndex += 1
             blockCounter = 1
-        l = len(dataset[i])
-        labels = np.zeros((l,9));    
-        labels[:,speakerIndex] = 1
+        if timestep == True:
+            l = len(dataset[i])
+            labels = np.zeros((l,9))   
+            labels[:,speakerIndex] = 1
+        else:
+            labels = np.zeros(9)    
+            labels[speakerIndex] = 1
         y.append(labels)
     return y
-
-# To be implemented when/if needded
-def proccess_y_timeseries(dataset):
-    pass
 
 ## this function does what the provided matlab dataloader does: one hot encoding for each timestep of the utterance
 def load_w_label_timestep():
@@ -64,8 +70,8 @@ def load_w_label_timestep():
     train_x = np.asarray(proccess_x(raw_train),dtype=object)
     test_x = np.asarray(proccess_x(raw_test),dtype=object)
     
-    train_y = np.asarray(proccess_train_y_timestep(train_x))
-    test_y = np.asarray(proccess_test_y_timestep(test_x))
+    train_y = np.asarray(proccess_train_y(train_x, timestep=True),dtype=object)
+    test_y = np.asarray(proccess_test_y(test_x, timestep=True),dtype=object)
 
     return [train_x, train_y, test_x, test_y]
 
@@ -75,12 +81,21 @@ def load_w_label_timeseries():
     raw_train = read_raw_file_lines(train_data_path)
     raw_test = read_raw_file_lines(test_data_path)
 
+    train_x = np.asarray(proccess_x(raw_train),dtype=object)
+    test_x = np.asarray(proccess_x(raw_test),dtype=object)
+    
+    train_y = np.asarray(proccess_train_y(train_x, timestep=False),dtype=object)
+    test_y = np.asarray(proccess_test_y(test_x, timestep=False),dtype=object)
+
+    return [train_x, train_y, test_x, test_y]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load train/test data in a specific format')
-    parser.add_argument('-labels_per_timestep', type=bool, default=False, help='if true each time step of the utterance has a label, otherwise the label is associated with the entire timeserise of the utterance.')
+    parser.add_argument('-labels_per_timestep', type=bool, default=False, help='if added each time step of the utterance has a label, otherwise the label is associated with the entire timeserise of the utterance.')
 
     args = parser.parse_args()
-
+    print(args.labels_per_timestep)
     if args.labels_per_timestep:
         print("Loading data with timestep labels")
         load_w_label_timestep()
