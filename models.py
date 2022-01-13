004258f36684bb572bd6db6deea49360e90aaefd
Emraldis <alfie.feltham@gmail.com>
2022-01-13 17:32:04 +0100

    Added initial GMLVQ code

--- models.py
+++ models.py
@@ -2,9 +2,17 @@ import preprocessing
 import argparse
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
 from sklearn import metrics
 import numpy as np
 
+#GMLVQ library
+import sklvq
+from sklvq import GMLVQ
+
+
+#K Means Classifier
 def kmeans(X_train, y_train):
     kmeans = KMeans(n_clusters=9, random_state=0).fit(X)
     kmeans.labels_
@@ -13,6 +21,7 @@ def kmeans(X_train, y_train):
 def knn(X_train, y_train):
     pass
 
+#Random Forest Classifier
 def randomForest(D, n_trees, split):
     X_train = D[0]
     y_train = D[1]
@@ -20,7 +29,7 @@ def randomForest(D, n_trees, split):
     y_test = D[3]
 
     model = RandomForestClassifier(n_estimators=n_trees, criterion=split)
-    
+
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -43,7 +52,30 @@ def run_RandomForest():
 
     #with MLflow record the parameretes and resutls
 
+def gmlvq(train_data, train_labels):
+    #temporarily hardcoded values
+    max_runs = 100
+    k = 2
+    step_size = np.array([0.1, 0.05])
+
+    model = GMLVQ(
+        distance_type="adaptive-squared-euclidian",
+        activation_type="identity",
+        solver_type="waypoint-gradient-descent",
+        solver_params={
+            "max_runs": max_runs,
+            "k": k,
+            "step_size": step_size
+        }
+        random_state=1428
+    )
+
+    model.fit(train_data, train_labels)
+
+def run_gmlvq(test_data, test_labels):
+    predicted_labels = model.predict(test_data)
+    print("GMLVQ:\n" + classification_report(test_labels, predicted_labels))
+    return(predicted_labels)
+
 if __name__ == '__main__':
     run_RandomForest()
-
-    
